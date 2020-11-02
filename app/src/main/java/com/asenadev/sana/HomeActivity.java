d7f3d00696be5a8339dae065583879ad19809495
khodealib <alib.ce.gu@gmail.com>
2020-11-02 05:44:39 -0800

    add login response model

--- app/src/main/java/com/asenadev/sana/HomeActivity.java
+++ app/src/main/java/com/asenadev/sana/HomeActivity.java
@@ -1,18 +1,90 @@
 package com.asenadev.sana;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 
 import com.asenadev.sana.R;
+import com.asenadev.sana.model.login.LoginResponse;
+import com.asenadev.sana.model.remote.ApiService;
+import com.asenadev.sana.utils.CustomTypefaceSpan;
+import com.google.android.material.navigation.NavigationView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private NavigationView navView;
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
 
+
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://asena-api.asenadev.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        apiService.login("1234516789","1234516789").enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if (response.body() != null) {
+                    Log.i(TAG, "onResponse: " + response.body().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                Log.i(TAG, "onFailure: " + t.getMessage());
+            }
+        });
+
+
+        navView =findViewById(R.id.nav_menu);
+        Menu m = navView.getMenu();
+        for (int i=0;i<m.size();i++) {
+            MenuItem mi = m.getItem(i);
+
+            //for aapplying a font to subMenu ...
+            SubMenu subMenu = mi.getSubMenu();
+            if (subMenu!=null && subMenu.size() >0 ) {
+                for (int j=0; j <subMenu.size();j++) {
+                    MenuItem subMenuItem = subMenu.getItem(j);
+                    applyFontToMenuItem(subMenuItem);
+                }
+            }
+
+            //the method we have create in activity
+            applyFontToMenuItem(mi);
+        }
+    }
+
+    private void applyFontToMenuItem(MenuItem mi) {
+        Typeface font = Typeface.createFromAsset(getAssets(), "font_regular.ttf");
+        SpannableString mNewTitle = new SpannableString(mi.getTitle());
+        mNewTitle.setSpan(new CustomTypefaceSpan("" , font), 0 , mNewTitle.length(),  Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        //mNewTitle.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), 0, mNewTitle.length(), 0); Use this if you want to center the items
+        mi.setTitle(mNewTitle);
     }
 }
--- app/src/main/java/com/asenadev/sana/model/remote/ApiService.java
+++ app/src/main/java/com/asenadev/sana/model/remote/ApiService.java
@@ -1,8 +1,10 @@
 package com.asenadev.sana.model.remote;
 
 import com.asenadev.sana.model.employee.Response;
+import com.asenadev.sana.model.login.LoginResponse;
 
 import io.reactivex.Single;
+import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
@@ -12,7 +14,7 @@ public interface ApiService {
 
     @FormUrlEncoded
     @POST("client/login")
-    Single<LoginResponse> login(@Field("username") String username, @Field("password") String password);
+    Call<LoginResponse> login(@Field("username") String username, @Field("password") String password);
 
 
     @GET("employee/list")
